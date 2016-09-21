23c7529d44f246aa9d1950bd9b16727434131e68
Nick Bragdon <nbragdon@bandwidth.com>
2016-09-21 09:41:04 -0400

    Updating the server code to work correctly

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
+/*
+ * Notice how the specific creation of a router is necessary here
+*/
+const sampleRouter = new Router();
 const PORT = 8431;
 
-app.use(async ctx => {
-  ctx.body = 'success!';
+/*
+ * This is used to get the total time a request takes to process
+*/
+app.use(async (ctx, next) => {
+  const startTime = Date.now();
+
+  await next();
+
+  const timeToProcess = Date.now() - startTime;
+  const fullUrl = `${ctx.protocol}://${ctx.host}${ctx.originalUrl}`;
+
+  console.log(`${ctx.method} ${fullUrl} - ${timeToProcess}`);
 });
+
+/*
+ * What do you think will happen here?
+ * What do you think will happen if you comment out the next(); line?
+ */
+app.use(async (ctx, next) => {
+  console.log('before next');
+  await next();
+  console.log('after next');
+});
+
+sampleRouter.get('/', async ctx => {
+  console.log('success!');
+
+  ctx.body = {
+    success: true
+  };
+});
+
+app.use(sampleRouter.routes());
 
 app.listen(PORT);
